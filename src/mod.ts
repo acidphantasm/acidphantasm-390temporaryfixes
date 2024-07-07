8f6071e19238518595ac7ff33d97ed4d8038deef
acidphantasm <127812106+acidphantasm@users.noreply.github.com>
2024-07-06 22:39:10 -0500

    actual code lol woops

--- src/mod.ts
+++ src/mod.ts
@@ -3,10 +3,9 @@ import { DependencyContainer } from "tsyringe";
 import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
 import { DatabaseServer } from "@spt/servers/DatabaseServer";
 import { IDatabaseTables } from "@spt/models/spt/server/IDatabaseTables";
-import { ItemHelper } from "@spt/helpers/ItemHelper";
-import { BaseClasses } from "@spt/models/enums/BaseClasses";
+import { ILogger } from "@spt/models/spt/utils/ILogger";
 
-class Mod implements IPostDBLoadMod
+class TemporaryFixes implements IPostDBLoadMod
 {
     public postDBLoad(container: DependencyContainer): void
     {
@@ -16,49 +15,26 @@ class Mod implements IPostDBLoadMod
         // Get all the in-memory json found in /assets/database
         const tables: IDatabaseTables = databaseServer.getTables();
 
-        // ---------------------------------------------------------
-        // example #1
-        // Make the LEDX item sellable on flea market
 
-        // Find the ledx item by its Id
-        const ledx = tables.templates.items["5c0530ee86f774697952d952"];
+        // Fix new figurines to be lootable
+        const restrictionsInRaid = tables.globals.config.RestrictionsInRaid;
+        const newFigurines = [
+            "66572b8d80b1cd4b6a67847f",
+            "66572be36a723f7f005a066e",
+            "66572cbdad599021091c611a",
+            "66572c82ad599021091c6118"
+        ]
 
-        // Update one of its properties to be true
-        ledx._props.CanSellOnRagfair = true;
-
-        // ---------------------------------------------------------
-        // example #2
-        // Get globals settings and set flea market min level to be 1
-        tables.globals.config.RagFair.minUserLevel = 1;
-
-        // ---------------------------------------------------------
-        // Example #3
-        // Loop over all magazines and make them weigh nothing
-
-        // Get ItemHelper ready to use
-        const itemHelper: ItemHelper = container.resolve<ItemHelper>("ItemHelper");
-
-        // Get all items in the database as an array so we can loop over them later
-        // tables.templates.items is a dictionary, the key being the items template id, the value being the objects data,
-        // we want to convert it into an array so we can loop over all the items easily
-        // Object.values lets us grab the 'value' part as an array and ignore the 'key' part
-        const items = Object.values(tables.templates.items);
-
-        // Use the itemHelper class to assist us in getting only magazines
-        // We are filtering all items to only those with a base class of MAGAZINE (5448bc234bdc2d3c308b4569)
-        const magazines = items.filter(x => itemHelper.isOfBaseclass(x._id, BaseClasses.MAGAZINE));
-
-        // Loop over all the magazines the above code found
-        for (const magazine of magazines)
+        for (const restriction in restrictionsInRaid)
         {
-            // Check the magazine has a weight property before we edit it
-            if (magazine._props.Weight)
+            const isIncluded =  newFigurines.some(itemID => restrictionsInRaid[restriction].TemplateId.includes(itemID))
+            if (isIncluded)
             {
-                // Set its weight to 0
-                magazine._props.Weight = 0;
+                restrictionsInRaid[restriction].MaxInLobby = 100;
+                restrictionsInRaid[restriction].MaxInRaid = 100;
             }
         }
     }
 }
 
-export const mod = new Mod();
+export const mod = new TemporaryFixes();

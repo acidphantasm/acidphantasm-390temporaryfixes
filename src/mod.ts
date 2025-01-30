83e0a80f1b1072f1e88e26c1d8ce42ad31948857
acidphantasm <acidphantasm@acidphantasm.com>
2025-01-29 19:41:07 -0600

    Update to 3.10.5 fixes

--- src/mod.ts
+++ src/mod.ts
@@ -2,19 +2,19 @@ import type { DependencyContainer } from "tsyringe";
 
 // SPT imports
 import type { IPreSptLoadMod } from "@spt/models/external/IPreSptLoadMod";
+import type { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
+import { DatabaseService } from "@spt/services/DatabaseService";
 import type { ILogger } from "@spt/models/spt/utils/ILogger";
 import type { VFS } from "@spt/utils/VFS";
 import type { ConfigServer } from "@spt/servers/ConfigServer";
 import type { ICoreConfig } from "@spt/models/spt/config/ICoreConfig";
+import { IItemConfig } from "@spt/models/spt/config/IItemConfig";
 import { ConfigTypes } from "@spt/models/enums/ConfigTypes";
 import { satisfies } from "semver";
 
-// Custom Classes
-import { MyCustomBotLevelGenerator } from "./MyCustomBotLevelGenerator";
-
 import path from "node:path";
 
-class TemporaryFixes implements IPreSptLoadMod
+class TemporaryFixes implements IPreSptLoadMod, IPostDBLoadMod
 {
     private static container: DependencyContainer;
 
@@ -23,12 +23,28 @@ class TemporaryFixes implements IPreSptLoadMod
         const logger = container.resolve<ILogger>("WinstonLogger");
 
         if (!this.validSptVersion(container)) {
-            logger.error('This version of Temporary Fixes was not made for your version of SPT. Disabling');
+            logger.error("This version of Temporary Fixes was not made for your version of SPT. Disabling");
             return;
-        }
+        }        
+    }
 
-        container.register<MyCustomBotLevelGenerator>("MyCustomBotLevelGenerator", MyCustomBotLevelGenerator);
-        container.register("BotLevelGenerator", { useToken: "MyCustomBotLevelGenerator" });
+    public postDBLoad(container: DependencyContainer): void 
+    {
+        const databaseService: DatabaseService = container.resolve<DatabaseService>("DatabaseService");
+        const pricesTable = databaseService.getTables().templates.prices;
+        const handbookTable = databaseService.getTables().templates.handbook;
+
+        // Set price of TerraGroup Labs residential unit keycard [Res. unit]
+        pricesTable["6711039f9e648049e50b3307"] = 165000;
+
+        // Set handbook price of TerraGroup Labs residential unit keycard [Res. unit]
+        handbookTable.Items.push(
+            {
+                "Id": "6711039f9e648049e50b3307",
+                "ParentId": "5c518ed586f774119a772aee",
+                "Price": 165000
+            }
+        )
     }
 
     /**
